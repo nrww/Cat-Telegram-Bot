719fee2b804446ac09e0e02b5f4e1ca2a11bf15d
nrww <narywow@gmail.com>
2024-06-04 18:14:57 +0500

    delete fastapi

--- main.py
+++ main.py
@@ -9,15 +9,12 @@ from data import *
 from handlers import common, cat_handler
 from midlewares import AlbumsMiddleware
 
-from fastapi import FastAPI
-from contextlib import asynccontextmanager
-from routes import root_router
 
 TOKEN = os.environ["TOKEN"]
 
 
 
-async def start_telegram():
+async def main():
     bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
     
     dp = Dispatcher(storage=MemoryStorage())
@@ -31,16 +28,6 @@ async def start_telegram():
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
-@asynccontextmanager
-async def lifespan(application: FastAPI):
-    asyncio.create_task(start_telegram())
-    yield
-    
-    
-app = FastAPI(lifespan=lifespan)
-app.include_router(root_router)
-
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8081)
+    asyncio.run(main())
